ccd55fb43102530690a8ef4b828c7a8bf5d8786a
Hydragyrum <hydragyrum@gmail.com>
2017-09-30 16:28:43 +0200

    Get neighbours

--- src/main/kotlin/life/model/Game.kt
+++ src/main/kotlin/life/model/Game.kt
@@ -2,19 +2,42 @@ package life.model
 
 class Game(val width: Int = 32, val height: Int = 32) {
 
-    private var state: Array<Boolean> = Array(width * height, {_ -> false})
+    private var state: Array<Boolean> = Array(width * height, { _ -> false })
 
-    fun cellAt(x: Int, y: Int): Boolean {
-        return state[calculateIndex(x, y)]
-    }
+    private val NEIGHBOURS = listOf(Point(-1, -1), Point(0, -1), Point(1, -1),
+            Point(-1, 0), Point(1, 0),
+            Point(-1, 1), Point(0, 1), Point(1, 1))
 
-    fun birth(x: Int, y: Int) {
-        state[calculateIndex(x, y)] = true
+    fun cellAt(x: Int, y: Int) = cellAt(Point(x, y))
+
+    fun cellAt(p: Point) : Boolean = state[calculateIndex(p)]
+
+    fun birth(x: Int, y: Int) = birth(Point(x, y))
+
+    fun birth(p: Point) {
+        state[calculateIndex(p)] = true
     }
 
     fun kill(x: Int, y: Int) {
         state[calculateIndex(x, y)] = false
     }
 
-    private fun calculateIndex(x: Int, y: Int) = x * width + y
+    fun clear() {
+        state.fill(false)
+    }
+
+    fun getLiveNeighbours(x: Int, y: Int): Int = getLiveNeighbours(Point(x, y))
+
+    fun getLiveNeighbours(p: Point): Int {
+        return NEIGHBOURS.map { p + it }
+                .filter { this.cellAt(it) }
+                .count()
+    }
+
+    private fun calculateIndex(x: Int, y: Int) = calculateIndex(Point(x, y))
+    private fun calculateIndex(p: Point) = ((p.x + width) % width) * width + ((p.y + height) % height)
+}
+
+class Point(val x: Int, val y: Int) {
+    operator fun plus(other: Point): Point = Point(x + other.x, y + other.y)
 }
--- src/test/kotlin/life/model/GameSpek.kt
+++ src/test/kotlin/life/model/GameSpek.kt
@@ -12,6 +12,10 @@ object GameSpek : Spek ({
     describe("A Game Board" ) {
         val board = Game(5, 5)
 
+        beforeEachTest {
+            board.clear()
+        }
+
         on("Creation") {
             it("Should be a 5 x 5 grid.") {
                 board.width `should equal` 5
@@ -26,11 +30,34 @@ object GameSpek : Spek ({
             }
         }
 
+        on("Birth a cell at point 1, 3 with a Point") {
+            board.birth(Point(1, 3))
+            it("Should make the point alive") {
+                board.cellAt(Point(1, 3))
+            }
+        }
+
         on("Kill cell at point 2, 2") {
             board.birth(2, 2)
             board.kill(2, 2)
             it("Should make point dead") {
                 board.cellAt(2, 2).`should be false`()
+            }
+        }
+
+        on("Get number of live neighbours") {
+            board.birth(1, 1)
+            board.birth(3, 3)
+            it( "Point 2, 2, should have 2 neighbours") {
+                board.getLiveNeighbours(2, 2) `should equal` 2
+            }
+        }
+
+        on("Get number of live neighbours from the edge") {
+            board.birth(1, 0)
+            board.birth(4, 4)
+            it("Point 0, 0 should have 2 neighbours") {
+                board.getLiveNeighbours(0, 0) `should equal` 2
             }
         }
     }
