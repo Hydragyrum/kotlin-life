6e4eb7e5d9e2d0333ab3be197fe21afd0ec95e71
Hydragyrum <hydragyrum@gmail.com>
2017-09-30 11:29:00 +0200

    Basic unit tests for the board

--- src/main/kotlin/life/model/Game.kt
+++ src/main/kotlin/life/model/Game.kt
@@ -1,3 +1,22 @@
 package life.model
 
-class Game(val width: Int = 32, val height: Int = 32)
+import com.sun.org.apache.xpath.internal.operations.Bool
+
+class Game(val width: Int = 32, val height: Int = 32) {
+
+    private var state: Array<Boolean> = Array(width * height, {_ -> false})
+
+    fun cellAt(x: Int, y: Int): Boolean {
+        return state[calculateIndex(x, y)]
+    }
+
+    fun birth(x: Int, y: Int) {
+        state[calculateIndex(x, y)] = true
+    }
+
+    fun kill(x: Int, y: Int) {
+        state[calculateIndex(x, y)] = false
+    }
+
+    private fun calculateIndex(x: Int, y: Int) = x * width + y
+}
--- src/test/kotlin/life/model/GameSpek.kt
+++ src/test/kotlin/life/model/GameSpek.kt
@@ -1,5 +1,7 @@
 package life.model
 
+import org.amshove.kluent.`should be false`
+import org.amshove.kluent.`should be true`
 import org.amshove.kluent.`should equal`
 import org.jetbrains.spek.api.Spek
 import org.jetbrains.spek.api.dsl.describe
@@ -14,6 +16,21 @@ object GameSpek : Spek ({
             it("Should be a 5 x 5 grid.") {
                 board.width `should equal` 5
                 board.height `should equal` 5
+            }
+        }
+
+        on("Birth a cell at point 2, 2." ) {
+            board.birth(2, 2)
+            it("Should make the point alive") {
+                board.cellAt(2, 2).`should be true`()
+            }
+        }
+
+        on("Kill cell at point 2, 2") {
+            board.birth(2, 2)
+            board.kill(2, 2)
+            it("Should make point dead") {
+                board.cellAt(2, 2).`should be false`()
             }
         }
     }
