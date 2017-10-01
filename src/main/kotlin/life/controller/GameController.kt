f0d55a0135ed42551507e135d9cc0732b5561605
Hydragyrum <hydragyrum@gmail.com>
2017-10-01 19:47:57 +0200

    Setup part of the Controller to setup the board

--- src/main/kotlin/life/controller/GameController.kt
+++ src/main/kotlin/life/controller/GameController.kt
@@ -4,5 +4,18 @@ import tornadofx.Controller
 import life.model.Game
 
 class GameController : Controller() {
-    val board = Game()
+    private val DEFAULT_WIDTH = 32
+    private val DEFAULT_HEIGHT = 32
+
+    private var board = Game(DEFAULT_WIDTH, DEFAULT_HEIGHT)
+
+    val width : Int
+    get() = board.width
+
+    val height : Int
+    get() = board.height
+
+    fun resetBoard(height: Int, width: Int) {
+        board = Game(width, height)
+    }
 }
